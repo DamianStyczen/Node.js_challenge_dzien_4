af42e71cf3616370ef2e31c01811231d408748b1
Styku0 <styku94@gmail.com>
2018-09-21 10:17:55 +0200

    Zadania zrobione

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,12 @@
 //Twój kod
+const http = require("http");
+
+const srv = http.createServer((req, res) => {
+    console.log("Hello World from Node");
+    console.log(req.headers['user-agent']);
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<strong>Hello world from Backend</strong>")
+})
+srv.listen(3000, ()=>{
+    console.log("Port 3000");
+})
